89e898eab3ee5c18e2caf0fdb1fb55ab998f4a7a
andjelamijatovic <andjelammijatovic@gmail.com>
2024-05-01 17:15:43 +0200

    Initial commit: add mvn dependencies, set up database configuration

---
+++ src/main/java/com/api/v2/tastytech/TastyTechApplication.java
@@ -0,0 +1,13 @@
+package com.api.v2.tastytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/v2/tastytech/TastyTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.v2.tastytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
