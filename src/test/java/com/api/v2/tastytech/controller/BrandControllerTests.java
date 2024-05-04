970dd05198dd08a9165b779e7600607a35d9b103
andjelamijatovic <andjelammijatovic@gmail.com>
2024-05-04 18:32:34 +0200

    Bug fig: Refactor @WebMvcTest annotation to target BrandController class

--- src/test/java/com/api/v2/tastytech/controller/BrandControllerTests.java
+++ src/test/java/com/api/v2/tastytech/controller/BrandControllerTests.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@WebMvcTest(BrandControllerTests.class)
+@WebMvcTest(BrandController.class)
 public class BrandControllerTests {
 
     @MockBean
